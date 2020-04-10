5afaff8fd01b80cd9c02dda519ab7f235e3fe102
DEV1 <manik@Manikanta>
2020-04-11 03:00:29 +0530

    hours is updated in test SimpleCode.java

--- FirstApplication/src/com/mnt/Controller/SimpleCode.java
+++ FirstApplication/src/com/mnt/Controller/SimpleCode.java
@@ -1,6 +1,7 @@
 package com.mnt.Controller;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class SimpleCode 
 {
@@ -8,5 +9,9 @@ public class SimpleCode
    {
 	   Calendar cal=Calendar.getInstance();
 	   int c=cal.getFirstDayOfWeek();
+	   System.out.println(c);
+	   Date date=new Date();
+	   System.out.println(date.getHours());
+	   System.out.println(date.getTime());
    }
 }
