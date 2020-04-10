c45ffde2e810115f6d2da0ef32ac0d64ee05719d
DEV <manik@Manikanta>
2020-04-11 02:42:53 +0530

    Basic directory structure created

---
+++ FirstApplication/src/com/mnt/Controller/SimpleCode.java
@@ -0,0 +1,12 @@
+package com.mnt.Controller;
+
+import java.util.Calendar;
+
+public class SimpleCode 
+{
+   public void getOutput()
+   {
+	   Calendar cal=Calendar.getInstance();
+	   int c=cal.getFirstDayOfWeek();
+   }
+}
---
+++ FirstApplication/src/com/mnt/Test/Test.java
@@ -0,0 +1,12 @@
+package com.mnt.Test;
+
+import com.mnt.Controller.SimpleCode;
+
+public class Test 
+{
+public static void main(String[] args) 
+{
+    SimpleCode sc=new SimpleCode();
+    sc.getOutput();
+}
+}
