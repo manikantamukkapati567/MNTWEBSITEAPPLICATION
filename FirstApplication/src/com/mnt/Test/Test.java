9a4b36d01416916ddfb81b316dde9270cfbc91f1
DEV <manik@Manikanta>
2020-04-11 03:36:52 +0530

    changes done in the test class

--- FirstApplication/src/com/mnt/Test/Test.java
+++ FirstApplication/src/com/mnt/Test/Test.java
@@ -5,5 +5,6 @@ public class Test
  public static void main(String[] args) 
  {
     System.out.println("this is the modification done by DEV");	
+    System.out.println("this is the output from the test class");
 }
 }
